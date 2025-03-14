17286f3d3d968d3edb43e329c844a0c9e754afed
SamueldeJesusdeOliveira <samuksjj@gmail.com>
2025-03-14 00:54:36 -0300

    Add project files.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2024, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
