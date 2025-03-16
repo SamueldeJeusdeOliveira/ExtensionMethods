e902b6146401a0bf3d3d76d18f19132da6dc763f
SamueldeJesusdeOliveira <samuksjj@gmail.com>
2025-03-16 01:31:18 -0300

    SegundaDemo

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -8,6 +8,11 @@ namespace ExtensionMethods
         {
             DateTime dt = new DateTime(2024, 11, 16, 8, 10, 45);
             Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students!";
+
+            Console.WriteLine(s1.Cut(10));
+
         }
     }
 }
